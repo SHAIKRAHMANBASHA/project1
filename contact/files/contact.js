51b65ee762ba6ec657419621af64a04d61777409
student-true=academy <rahmanbasha091@gmail.com>
2026-03-26 01:36:28 +0530

    add contact

---
+++ contact/files/contact.js
@@ -0,0 +1,8 @@
+//contact add box coding here
+
+var plus_icon = document.getElementById("plus_icon");
+plus_icon.onclick = function()
+{
+    var contact_bg = document.getElementById("contact_bg");
+    contact_bg.style.display ="block";
+}
